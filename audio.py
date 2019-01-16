0b4f3b4deb5cb709098c4a7dcdb08a7ce7a31648
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-01-16 13:51:42 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -612,7 +612,7 @@ class Fun:
 @bot.event
 async def on_member_join(member):
     channel = get(member.server.channels, name="welcome")
-    embed = discord.Embed(title='Yo, welcome!', description=welcomemsg_gangsta, colour=0x7ED6DE)
+    embed = discord.Embed(title='Yo, welcome!', description="welcome", colour=0x7ED6DE)
    
     embed.set_author(name=member.name, icon_url=member.avatar_url)
     await bot.send_message(channel, embed=embed)
