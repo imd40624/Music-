ca37ec6287a8965cfa108bf653736fa3cbc3f560
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-01-17 11:46:48 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -267,16 +267,7 @@ async def mute(ctx, member: discord.Member):
 	
 	
         
-@bot.command(pass_context=True)
-async def unmute(ctx, member: discord.Member):
-     if ctx.message.author.server_permissions.administrator or ctx.message.author.id == '455500545587675156':
-	role = discord.utils.get(member.server.roles, name='Muted')
-	await bot.remove_roles(member, role)
-        embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
-        await bot.say(embed=embed)
-     else:
-        embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
-        await bot.say(embed=embed)
+
        
         
         
