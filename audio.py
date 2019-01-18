acfc05f7a8bc7ddcccec1ddbad82641ea6031653
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-01-18 11:05:04 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -553,73 +553,7 @@ async def dice( con, min1=1, max1=6):
     await bot.send_message(con.message.channel, "**{}**".format(r))
 
 
-class Fun:
 
-    def __init__(self, bot):
-        self.bot = bot
-
-    def getdog():
-        dog = urllib.request.urlopen('https://random.dog/woof')
-        dog = str(dog.read())
-        dog = dog[2:]
-        dog = dog[:len(dog) - 1]
-        return dog
-
-    def getcat():
-        cat = urllib.request.urlopen('https://random.cat/meow')
-        cat = str(cat.read())
-        cat = cat[11:]
-        cat = cat[:len(cat) - 3]
-        cat = cat.replace("\\", "")
-        return cat
-
-    @bot.command(pass_context=True)
-    async def dog(self, ctx): # !!dog
-        """Gets a random dog from http://random.dog"""
-        await self.bot.send_typing(ctx.message.channel)
-        color = discord.Color.default()
-        if ctx.message.server is not None:
-            color = ctx.message.server.me.color
-        embed = discord.Embed(color=color)
-        embed.title = "Random Dog"
-
-        dog = "null"
-        while 1:
-            dog = Fun.getdog()
-            if not dog.endswith(".mp4"):
-                break
-        embed.set_footer(text="{}".format("http://random.dog/" + str(dog)))
-        embed.set_image(url="http://random.dog/" + str(dog))
-        await self.bot.send_message(ctx.message.channel, embed=embed)
-
-    @bot.command(pass_context=True)
-    async def cat(self, ctx): # !!cat
-        """Gets a random cat from http://random.cat"""
-        await self.bot.send_typing(ctx.message.channel)
-        color = discord.Color.default()
-        if ctx.message.server is not None:
-            color = ctx.message.server.me.color
-        embed = discord.Embed(color=color)
-        embed.title = "Random Cat"
-        cat = "null"
-        while 1:
-            cat = Fun.getcat()
-            if not cat.endswith(".mp4"):
-                break
-        embed.set_footer(text="{}".format(str(cat)))
-        embed.set_image(url=str(cat))
-        await self.bot.send_message(ctx.message.channel, embed=embed)
-
-    @bot.command(name="8ball")
-    async def _8ball(self, *, question : str):
-        """Ask a question."""
-        responses = [["Signs point to yes.", "Yes.", "Without a doubt.", "As I see it, yes.", "You may rely on it.", "It is decidedly so.", "Yes - definitely.", "It is certain.", "Most likely.", "Outlook good."],
-        ["Reply hazy, try again.", "Concentrate and ask again.", "Better not tell you now.", "Cannot predict now.", "Ask again later."],
-        ["My sources say no.", "Outlook not so good.", "Very doubtful.", "My reply is no.", "Don't count on it."]]
-        if "?" in question:
-            await self.bot.say(":8ball:" + random.choice(random.choice(responses)))
-        else:
-            await self.bot.say("That doesn't look like a question.")
 
 	
 @bot.event
@@ -646,7 +580,219 @@ async def on_member_remove(member):
     await bot.send_message(channel, embed=embed)
 
 
-        
+   
+	
+class Fun:
+    def __init__(self,bot):
+        self.bot=bot
+
+
+@bot.command(pass_context=True)
+async def dog(self,con):
+        r = rq.Session().get('https://random.dog/woof.json').json()
+        emb = discord.Embed(title='Dog')
+        emb.set_image(url=r['url'])
+        await self.bot.send_message(con.message.channel, embed=emb)
+
+
+@bot.command(pass_context=True)
+async def say(self,con, *, msg):
+        message = await self.bot.send_message(con.message.channel, "{}".format(msg))
+        await asyncio.sleep(120)
+        await self.bot.delete_message(message)
+
+
+@bot.command(pass_context=True)
+async def randomshow(self,con):
+        session = rq.Session()
+        url = 'https://tv-v2.api-fetch.website/random/show'
+        r = session.get(url).text
+        r_json = json.loads(r)
+        name = r_json['title']
+        year = r_json['year']
+        img = r_json['images']['poster']
+        await self.bot.send_message(con.message.channel, "**Name**: {}\n**Year**: {}\n**Poster**: {}".format(name, year, img))
+
+
+@bot.command(pass_context=True)
+async def catfact(self,con):
+        session = rq.Session()
+        fact_id = random.randint(0, 127)
+        r = session.get(
+            'https://jsonblob.com/api/d02645c2-151b-11e9-8960-c9ff29aada09')
+        if r.status_code != 200:
+            await self.bot.send_message(con.message.channel, "**Something went wrong please try again later**")
+        if r.status_code == 200:
+            try:
+                await self.bot.send_message(con.message.channel, "**{}**\n**Fact ID** `{}`".format(r.json()['animals']['cats'][fact_id], fact_id))
+            except:
+                await self.bot.send_message(con.message.channel, "**Something went wrong while sending the fact\nPlease try again later**")
+
+
+@bot.command(pass_context=True)
+@bot.cooldown(rate=1, per=2, type=commands.BucketType.channel)
+async def randomanime(self,con):
+        session = rq.Session()
+        """GENERATES A RANDOM ANIME TITLE WITH 10 SECOND COOL DOWN. EX: s.randomanime"""
+        r = rq.get('https://tv-v2.api-fetch.website/random/anime').json()
+        title = r['title']
+        mal_id = r['mal_id']
+        genres = r['genres']
+        url2 = 'https://api.jikan.moe/anime/{}/stats/'.format(mal_id)
+        r2 = session.get(url2).text
+        r2j = json.loads(r2)
+        summary = r2j['synopsis']
+        await self.bot.send_message(con.message.channel, "**Title**: {}\n**Genres**: {}\n**Synopsis**: {}".format(title, genres, summary))
+
+
+@bot.command(pass_context=True)
+async def randommovie(self,con):
+        session = rq.Session()
+        """GENERATES A RANDOM MOVIE TITLE. EX: s.randommovie"""
+        movie = session.get('https://tv-v2.api-fetch.website/random/movie')
+        if movie.status_code == 200:
+            rest = movie.text
+            rq_json = json.loads(rest)
+            title = rq_json['title']
+            summary = rq_json['synopsis']
+            runtime = rq_json['runtime']
+            genres = rq_json['genres']
+            gen = " ".join(genres[1:])
+            await self.bot.send_message(con.message.channel, "**Title**: {}\n**Genres**: {}\n**Length*: {} Minutes\n**Synopsis**: {}".format(title, gen, runtime, summary))
+
+
+@bot.command(pass_context=True)
+async def cat(self,con):
+        r = rq.Session().get('http://aws.random.cat/meow').json()
+        emb = discord.Embed(title='Cat')
+        emb.set_image(url=r['file'])
+        await self.bot.send_message(con.message.channel, embed=emb)
+
+
+ @bot.command(pass_context=True)
+ async def cookie(self,con, user: discord.Member):
+        amount = random.randint(1, 15)
+        await self.bot.send_message(con.message.channel, "{0} you got {2} cookies from {1}".format(user.mention, con.message.author.name, amount))
+
+
+ @bot.command(pass_context=True)
+ async def neko(self,con, *, nsfw='None'):
+        if nsfw.lower() == 'nsfw':
+            session = rq.Session()
+            r = session.get(
+                'https://nekos.moe/api/v1/random/image?count=1&nsfw=true').json()
+            id = r['images'][0]['id']
+            msg = discord.Embed(title='Neko')
+            msg.set_image(url='https://nekos.moe/image/{}'.format(id))
+            try:
+                msg.set_footer(text='Artist: {}'.format(r['images'][0]['artist']))
+            except KeyError:
+                pass
+            try:
+                await self.bot.send_message(con.message.channel, embed=msg)
+            except:
+                await self.bot.send_message(con.message.author, embed=msg)
+        elif nsfw == 'None' and 'nsfw' not in nsfw.lower():
+            session = rq.Session()
+            r = session.get(
+                'https://nekos.moe/api/v1/random/image?count=1&nsfw=false').json()
+            id = r['images'][0]['id']
+            msg = discord.Embed(title='Neko')
+            msg.set_image(url='https://nekos.moe/image/{}'.format(id))
+            try:
+                msg.set_footer(text='Artist: {}'.format(r['images'][0]['artist']))
+            except:
+                pass
+            try:
+                await self.bot.send_message(con.message.channel, embed=msg)
+            except:
+                await self.bot.send_message(con.message.author, embed=msg)
+
+		
+@bot.command(pass_context=True)
+async def bunnyfact(self,con):
+        session = rq.Session()
+        fact_id = random.randint(0, 17)
+        r = session.get(
+            'https://jsonblob.com/api/ea1a1a28-151b-11e9-8960-6d585dac6621')
+        if r.status_code != 200:
+            try:
+                await self.bot.send_message(con.message.channel, "Somethign went wrong, please try again later")
+            except:
+                await self.bot.send_message("Something went wrong, please try again later")
+        if r.status_code == 200:
+            try:
+                await self.bot.send_message(con.message.channel, "**{}**\n**Fact ID** `{}`".format(r.json()['animals']['bunny'][fact_id], fact_id))
+            except:
+                await self.bot.send_message("**{}**\n**Fact ID** `{}`".format(r.json()['animals']['bunny'][fact_id], fact_id))
+
+
+@bot.command(pass_context=True)
+async def pifact(self,con):
+        session = rq.Session()
+        fact_id = random.randint(0, 49)
+        r = session.get(
+            'https://jsonblob.com/api/ea1a1a28-151b-11e9-8960-6d585dac6621')
+        if r.status_code != 200:
+            await self.bot.send_message(con.message.channel, "**Something went wrong while trying to get the fact\nPlease try again later**")
+        if r.status_code == 200:
+            try:
+                await self.bot.send_message(con.message.channel, "**{}**\n**Fact ID** `{}`".format(r.json()['math']['pi'][fact_id], fact_id))
+            except:
+                await self.bot.send_message(con.message.channel, "**Something went wrong while trying to send the fact\nPlease try again later**")
+
+
+@bot.command(pass_context=True)
+async def dogfact(self,con):
+        session = rq.Session()
+        fact_id = random.randint(0, 100)
+        r = session.get(
+            'https://jsonblob.com/api/ea1a1a28-151b-11e9-8960-6d585dac6621')
+        if r.status_code != 200:
+            await self.bot.send_message(con.message.channel, "**Somethign went wrong retrieving the fact\nPlease try again later.**")
+        if r.status_code == 200:
+            try:
+                await self.bot.send_message(con.message.channel, "**{}**\n**Fact ID** `{}`".format(r.json()['animals']['dogs'][fact_id], fact_id))
+            except:
+                await self.bot.send_message(con.message.channel, "**Something went wrogn while trying to send the fact\nPlease try again later.**")
+
+
+
+@bot.command(pass_context=True)
+@commands.cooldown(rate=10, per=1, type=commands.BucketType.channel)
+async def game(self,con, *,msg):
+        """CHANGES THE PLAYING STATUS OF THE BOT. EX: s.game OSU!"""
+        await self.bot.change_presence(game=discord.Game(name=msg))
+
+	
+@bot.command(pass_context=True)
+@commands.cooldown(rate=10, per=1, type=commands.BucketType.channel)
+async def listening(self,con, *,msg):
+        await self.bot.change_presence(game=discord.Game(name=msg,type=2))
+
+
+@bot.command(pass_context=True)
+@commands.cooldown(rate=10, per=1, type=commands.BucketType.channel)
+async def watching(self,con, *,msg):
+        await self.bot.change_presence(game=discord.Game(name=msg,type=3))
+
+
+@bot.command(pass_context=True)
+async def bunny(self,con):
+        msg = discord.Embed(title='Bunny')
+        msg.set_image(url='https://www.kurusaki.com/Rab/{}.jpg'.format(random.randint(1, 89)))
+        await self.bot.send_message(con.message.channel, embed=msg)	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
