2fa1a1fa81dd807420cdd4fad22baad9b560787c
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-01-20 16:36:04 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -259,7 +259,7 @@ async def mute(ctx, member: discord.Member):
         await bot.add_roles(member, role)
         embed=discord.Embed(title="User Muted!", description="**{0}** was muted by **{1}**!".format(member, ctx.message.author), color=0xff00f6)
         await bot.say(embed=embed)
-    else:
+     else:
         embed=discord.Embed(title="Permission Denied.", description="You don't have permission to use this command.", color=0xff00f6)
         await bot.say(embed=embed)	
 	
