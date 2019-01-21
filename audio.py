0a090211181a250c5f2179c4eee5edc80c893db8
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-01-21 23:04:04 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -809,12 +809,12 @@ async def deletelinks_off(ctx):
 	
 @bot.event
 async def on_message_delete(message):
-    channel = get(member.server.channels, name="logs")
+    channel = get(message.server.channels, name="logs")
     fmt = '{0.author.name} has deleted the message:\n{0.content}'
     embed = discord.Embed(title='message deleted', description=fmt.format(message), colour=0xff00f6)
     embed.set_author(name=member.name, icon_url=member.avatar_url)
 
-    await bot.send_message(message.channel, embed=embed)
+    await bot.send_message(channel, embed=embed)
 	
 	
 	
