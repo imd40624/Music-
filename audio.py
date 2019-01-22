8076bc13f392b366f38d48af88876c494c0ecf77
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-01-22 23:26:28 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -825,7 +825,7 @@ async def on_message_edit(before, after):
     member = before.author
     channel = get(before.server.channels, name="logs")
     leavemsgdebug = '[{0.content}] -> [{1.content}]'.format(before, after)
-    usermsg = "{0} <{1}> ({2}) | {3}".format(member, member.id, before.channel.name).replace("'", "")
+    usermsg = "{0} <{1}> ({2})".format(member, member.id, before.channel.name).replace("'", "")
     embed = discord.Embed(title='Message Edited', description=leavemsgdebug, colour=0xF46900)
     embed.set_author(name=usermsg, icon_url=member.avatar_url)
     await bot.send_message(channel, embed=embed)
