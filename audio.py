2bdcb7a509359fe76d301d9abc27159181c9f962
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-02-12 22:56:23 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -1400,6 +1400,56 @@ async def happybirthday(ctx, *, msg = None):
     return
 
 
+
+@bot.event
+async def on_member_join(member):
+    with open("con.json", "r") as f:
+        users = json.load(f)
+
+        await update_data(users, member)
+
+        with open("con.json", "w") as f:
+            json.dump(users, f)
+
+@bot.event
+async def on_message(message):
+    with open("con.json", "r") as f:
+        users = json.load(f)
+
+        if message.author.bot:
+            return
+        else:
+            await update_data(users, message.author)
+            number = random.randint(5,10)
+            await add_experience(users, message.author, number)
+            await level_up(users, message.author, message.channel)
+
+        with open("con.json", "w") as f:
+            json.dump(users, f)
+    await bot.process_commands(message)
+
+async def update_data(users, user):
+    if not user.id in users:
+        users[user.id] = {}
+        users[user.id]["experience"] = 0
+        users[user.id]["level"] = 1
+
+async def add_experience(users, user, exp):
+    users[user.id]["experience"] += exp
+
+async def level_up(users, user, channel):
+    experience = users[user.id]["experience"]
+    lvl_start = users[user.id]["level"]
+    lvl_end = int(experience ** (1/4))
+
+    if lvl_start < lvl_end:
+        await bot.send_message(channel, f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
+        users[user.id]["level"] = lvl_end
+    
+
+
+
+
 	
 @bot.command(pass_context=True)
 async def embed(ctx):
