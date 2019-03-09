f030aad03b931a340c240318230de9a10f5e908c
imd40624 <46713870+imd40624@users.noreply.github.com>
2019-03-09 23:23:03 +0530

    Update audio.py

--- audio.py
+++ audio.py
@@ -1325,33 +1325,40 @@ async def on_message(message):
 		
         if message.author.bot:
             return
+        if message.channel.is_private:
+            return
         else:
-            await update_data(users, message.author)
+            await update_data(users, message.author, message.server)
             number = random.randint(5,10)
-            await add_experience(users, message.author, number)
-            await level_up(users, message.author, message.channel)
+            await add_experience(users, message.author, number, message.server)
+            await level_up(users, message.author, message.channel, message.server)
 
         with open("con.json", "w") as f:
             json.dump(users, f)
     await bot.process_commands(message)
 
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]["experience"] = 0
-        users[user.id]["level"] = 1
+async def update_data(users, user, server):
+    if not user.id + "-" + server.id in users:
+        users[user.id + "-" + server.id] = {}
+        users[user.id + "-" + server.id]["experience"] = 0
+        users[user.id + "-" + server.id]["level"] = 1
+        users[user.id + "-" + server.id]["last_message"] = 0
 
-async def add_experience(users, user, exp):
-    users[user.id]["experience"] += exp
+async def add_experience(users, user, exp, server):
+    if time.time() - users[user.id + "-" + server.id]["last_message"] > 5: 
+        users[user.id + "-" + server.id]["experience"] += exp
+        users[user.id + "-" + server.id]["last_message"] = time.time()
+    else:
+        return
 
-async def level_up(users, user, channel):
-    experience = users[user.id]["experience"]
-    lvl_start = users[user.id]["level"]
+async def level_up(users, user, channel, server):
+    experience = users[user.id + "-" + server.id]["experience"]
+    lvl_start = users[user.id + "-" + server.id]["level"]
     lvl_end = int(experience ** (1/4))
 
     if lvl_start < lvl_end:
         await bot.send_message(channel, f":tada: Congrats {user.mention}, you levelled up to level {lvl_end}!")
-        users[user.id]["level"] = lvl_end
+        users[user.id + "-" + server.id]["level"] = lvl_end
             	
 		
 		
